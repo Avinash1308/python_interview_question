b0d5e37403dca9557bae42d3b160b837130d3468
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-06-26 11:19:28 +0530

    Create merge_two_list_and_find_median.py

---
+++ merge_two_list_and_find_median.py
@@ -0,0 +1,16 @@
+list1=[1,3,5,7]
+list2=[2,4,6,8]
+combined_list=(list1+list2)
+print(combined_list)
+sorted_list=sorted(combined_list)
+print(sorted_list)
+length=len(sorted_list)
+print(length)
+print(length//2)
+if(length%2==0):
+  median=(sorted_list[length//2+1]+sorted_list[length//2])/2
+else:
+  median=sorted_list[length//2]
+print(median)
+# output
+# 5.5
